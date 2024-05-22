3bcc7e5ca4750a6ddd04be1cd1182bd2e7fcd538
GreeningSiren <greeningsiren@gmail.com>
2024-05-22 17:00:08 +0300

    feat: 🎸 it's now a progressive web app :)
    Emi tam mnogo tutoriqli izgledah

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/download/"
+    plugins: [react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            manifest: {
+                theme_color: "#05dcaa",
+                background_color: "#242424",
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        purpose: "maskable",
+                        sizes: "512x512",
+                        src: "icon512_maskable.png",
+                        type: "image/png"
+                    },
+                    {
+                        purpose: "any",
+                        sizes: "512x512",
+                        src: "icon512_rounded.png",
+                        type: "image/png"
+                    }],
+                name: "GreeningSiren's Download Center",
+                short_name: "Download Center",
+            },
+        })],
+    base: "/download/"
 })
