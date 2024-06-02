ac6059048f9f859f8fd6eb5b80f6769b083fb96e
GreeningSiren <greeningsiren@gmail.com>
2024-06-02 18:27:01 +0300

    test: 💍 million lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,44 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import {VitePWA} from "vite-plugin-pwa";
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import million from "million/compiler";
+import MillionLint from "@million/lint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-            manifest: {
-                theme_color: "#05dcaa",
-                background_color: "#242424",
-                icons: [
-                    {
-                        src: 'android-chrome-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        purpose: "maskable",
-                        sizes: "512x512",
-                        src: "icon512_maskable.png",
-                        type: "image/png"
-                    },
-                    {
-                        purpose: "any",
-                        sizes: "512x512",
-                        src: "icon512_rounded.png",
-                        type: "image/png"
-                    }],
-                name: "GreeningSiren's Download Center",
-                short_name: "GreeningSiren's Download Center",
-            },
-        })],
-    base: "/download/"
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        theme_color: "#05dcaa",
+        background_color: "#242424",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+        name: "GreeningSiren's Download Center",
+        short_name: "GreeningSiren's Download Center",
+      },
+    }),
+    million.vite({ auto: true }),
+    MillionLint.vite(),
+  ],
+  base: "/download/",
+});
